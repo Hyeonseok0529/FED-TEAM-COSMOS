ce5543dbbd0a89ece61b0addd729a0a433b354b9
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-16 09:56:50 +0900

    소스 업뎃

---
+++ 02.구현소스/js/sub.js
@@ -0,0 +1,22 @@
+// sub-js //
+
+// 갤러리 사진클릭시 원본보기//
+// 이벤트 대상 : .gallery > div
+// 변경대상: .smenu-contbox img
+const $smenuImg = $(".smenu-contbox");
+const $smenuImgbox = $(".smenu-imgbox");
+
+// 닫기버튼 셋팅
+// $(".close").click(() => $(".gallery-smenu").fadeOut());
+
+$('.gallery img').click(function(){
+// console.log("미리보기!")
+
+// 1. 클릭된 박스의 이미지 읽어오기
+let currImg = $(this).attr("src");/* .find(''); */
+console.log(currImg)
+
+// 2. 읽어온 내용을 서브 컨텐츠 박스에 넣기
+$smenuImgbox.html(`<img src="${currImg}" alt="이미지"></img>`);
+
+}) ///// click /////
