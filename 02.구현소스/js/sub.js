739be63ee3d5718bc1597a90bf51c9161d06a836
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-20 10:28:13 +0900

    스크롤 등장애니 적용중
    스크롤 등장 초기화 클래스 적용시켜야 함.

--- 02.구현소스/js/sub.js
+++ 02.구현소스/js/sub.js
@@ -1,5 +1,8 @@
 // sub-js //
 
+// 나의 함수 불러오기
+import myFn from "./my_function.js";
+
 // 부드러운 스크롤 함수 불러오기
 import startSS from "./smoothScroll23.js";
 
@@ -94,6 +97,48 @@ setTyped(
   8
 );
 
-// setTyped("#typed2",[
-//       `gggggggg\n gggggggggggggggg`,
-//     ],100);
+// 타이핑 스크롤 액션 //////////////////////////////////////////
+
+// 1. 대상선정:
+// (1) 이벤트 대상 : window
+// (2) 변경 대상 : .scroll-act
+const scrollAct = myFn.qsa(".scroll-act");
+// 요소에 트랜지션
+
+// console.log("대상:", scrollAct)
+
+// 스크롤 등장요소의 위치값 담기
+// ooffsetTop은 맨 위에서부터 요소의 위치값
+// 배열변수에 순서대로 담는다.
+const posEl = [];
+scrollAct.forEach((el, idx) => (posEl[idx] = el.offsetTop))
+
+// console.log("위치값:", posEl);
+
+// 2. 이벤트 설정하기 //
+// (1) 스크롤시 요소등장 함수호출
+myFn.addEvt(window, "scroll", showEl);
+
+// (2) 섹션에 따라 행성 이동 함수 호출 --> 추후 추가
+// myFn.addEvt(window, "scroll", movePlanet);
+
+// 기준값 만들기 : 화면 높이값을 사용(화면의 2/3)
+const CRITERIA = (window.innerHeight / 3) * 2;
+// console.log("기준값:", CRITERIA);
+
+// 3. 함수만들기 //
+// (1) 요소 등장 함수
+function showEl() {
+  // (1) 함수호출확인
+  // console.log("나야나!!",window.scrollY);
+
+  scrollAct.forEach((el) => {
+    // 각 등장요소의 바운딩 top값
+    let bcrVal = myFn.getBCR(el);
+    // console.log("등장요소 바운딩 top값은?:",bcrVal,el.getBoundingClientRect());
+
+    // 화면의 2/3위치에서 클래스 넣기(등장)
+    if (bcrVal < CRITERIA) el.classList.add()
+  })
+}
+
