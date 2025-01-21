ad0715efc974f08b465dd1374d7ddbcc744bd231
Tom B. Parker <tombap8@hotmail.com>
2025-01-21 12:14:26 +0900

    ss

--- 02.구현소스/js/sub.js
+++ 02.구현소스/js/sub.js
@@ -6,6 +6,15 @@ import myFn from "./my_function.js";
 // 부드러운 스크롤 함수 불러오기
 import startSS from "./smoothScroll23.js";
 
+import planetData from "./data_sub.json" with{type:'json'};
+console.log(planetData);
+
+// 행성키 변수 : url?planet=venus
+// let planetKey = location.search.split('=')[1];
+let planetKey = 'mars';
+const selData = planetData[planetKey];
+console.log(selData);
+
 // 부드러운 스크롤 함수호출
 startSS();
 
@@ -36,6 +45,8 @@ $(".gallery img").click(function () {
 
 $closeBtn.click(() => $gallerySmenu.fadeOut(300));
 
+const rangeId = ["typed2", "typed3", "typed5", "typed7", "typed9"];
+
 function setTyped(sName, sCont, spVal) {
   new Typed(sName, {
     strings: sCont,
@@ -47,43 +58,65 @@ function setTyped(sName, sCont, spVal) {
     // 반복여부(true/false)
     loop: false,
     startDelay: 2000,
+
+    onComplete: (self) => {
+      console.log(7777, self.el.id);
+      if (rangeId.includes(self.el.id)) {
+        self.cursor.style.display = "none";
+      }
+    },
   }); ////// typed /////////////
 }
 
+const startLetterFn = {
+  "first-area": () => {
+    // 1. first area 파트
+    setTyped(
+      "#typed1",
+      [
+        selData.first
+        // `^1000The smallest planet in our solar system and^100\nnearest to the Sun, Mercury is only slightly ^100\nlarger than Earth's Moon.`,
+      ],
+      8
+    );
+    setTyped("#typed2", [
+      planetKey
+      // `Mercury`
+    ], 40);
+  },
+  "introduction-area": () => {
+    // 2. Introduction 파트
+    setTyped("#typed3", [`introduction`], 40);
+    setTyped(
+      "#typed4",
+      [
+        `^1000surface temperatures : 800°F(430°C) ~ -290°F(-180°C)^100\nthe fastest planet, zipping around the Sun every^100\n88 Earth days.`,
+      ],
+      8
+    );
+  },
+  "Namesake-area": () => {
+    // 3. Namesake 파트
+    setTyped("#typed5", [`Namesake`], 40);
+    setTyped(
+      "#typed6",
+      [`^1000named for the swiftest of the ancient Roman gods.`],
+      8
+    );
+  },
+  "potential-area": () => {
+    // 4. Potential for Life 파트
+    setTyped("#typed7", [`Potential for Life`], 40);
+    setTyped(
+      "#typed8",
+      [
+        `^1000Mercury's environment is not conducive to life as we know it. \nThe temperatures and solar radiation that characterize this planet\nare most likely too extreme for organisms to adapt to.`,
+      ],
+      8
+    );
+  },
+};
 
-
-
-
-
-
-
-// 2. Introduction 파트
-setTyped("#typed3", [`introduction`], 40);
-setTyped(
-  "#typed4",
-  [
-    `^1000surface temperatures : 800°F(430°C) ~ -290°F(-180°C)^100\nthe fastest planet, zipping around the Sun every^100\n88 Earth days.`,
-  ],
-  8
-);
-// 3. Namesake 파트
-setTyped("#typed5", [`Namesake`], 40);
-setTyped(
-  "#typed6",
-  [
-    `^1000named for the swiftest of the ancient Roman gods.`,
-  ],
-  8
-);
-// 4. Potential for Life 파트
-setTyped("#typed7", [`Potential for Life`], 40);
-setTyped(
-  "#typed8",
-  [
-    `^1000Mercury's environment is not conducive to life as we know it. \nThe temperatures and solar radiation that characterize this planet\nare most likely too extreme for organisms to adapt to.`,
-  ],
-  8
-);
 // 5. Structure 파트
 setTyped("#typed9", [`Structure`], 40);
 setTyped(
@@ -108,7 +141,7 @@ const scrollAct = myFn.qsa(".scroll-act");
 // ooffsetTop은 맨 위에서부터 요소의 위치값
 // 배열변수에 순서대로 담는다.
 const posEl = [];
-scrollAct.forEach((el, idx) => (posEl[idx] = el.offsetTop))
+scrollAct.forEach((el, idx) => (posEl[idx] = el.offsetTop));
 
 // console.log("위치값:", posEl);
 
@@ -123,12 +156,12 @@ myFn.addEvt(window, "scroll", showEl);
 const CRITERIA = (window.innerHeight / 3) * 2;
 // console.log("기준값:", CRITERIA);
 
-
 // 타이핑 애니메이션 한번만 적용하기 //
 let typing = false;
 // 타이핑 효과 시간상수
 const TIME_TYPING = 3000;
 
+let beforeId = [];
 
 // 3. 함수만들기 //
 // (1) 요소 등장 함수
@@ -137,26 +170,23 @@ function showEl() {
   // console.log("나야나!!",window.scrollY);
 
   // (1-2) 타이핑 애니 한번만 적용하기
-  if (typing) return;// 함수 나가기
-  typing = true;
+  // if (typing) return;// 함수 나가기
+  // typing = true;
   scrollAct.forEach((el) => {
     // 각 등장요소의 바운딩 top값
     let bcrVal = myFn.getBCR(el);
     // console.log("등장요소 바운딩 top값은?:",bcrVal,el.getBoundingClientRect());
 
     // 화면의 2/3위치에서 클래스 넣기(등장)
-    if (bcrVal < CRITERIA){
-    // typed 플러그인 적용하기!
-    // 1. 첫 등장 파트
-    setTyped(
-      "#typed1",
-      [
-        `^1000The smallest planet in our solar system and^100\nnearest to the Sun, Mercury is only slightly ^100\nlarger than Earth's Moon.`,
-      ],
-      8
-    );
-    setTyped("#typed2", [`Mercury`], 40)}
-    else return;
+    if (bcrVal < CRITERIA) {
+      if (beforeId.includes(el.id)) return;
+      beforeId.push(el.id);
+      console.log(el.id);
+
+      startLetterFn[el.id]();
+
+      // typed 플러그인 적용하기!
+      // 1. 첫 등장 파트
+    }
   }); ////// forEach /////
 } ////// showEl함수 //////
-
