cc1ce7dcddd23fe07dc060a74cc46f95e9e95143
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-24 16:17:22 +0900

    타이핑 애니 수정중

--- 02.구현소스/js/sub.js
+++ 02.구현소스/js/sub.js
@@ -18,6 +18,20 @@ console.log(selData);
 // 부드러운 스크롤 함수호출
 startSS();
 
+// JSON데이터 HTML요소선택
+const Introduction = document.getElementById('introduction-data');
+const namesake = document.getElementById('namesake-data');
+const potential = document.getElementById('potential-data');
+const structure = document.getElementById('structure-data');
+
+// 데이터 삽입
+Introduction.textContent = selData.introduction
+namesake.textContent = selData.namesake
+potential.textContent = selData.potential
+structure.textContent = selData.structure
+
+
+
 // 갤러리 사진클릭시 원본보기//
 // 이벤트 대상 : .gallery > div
 // 변경대상: .smenu-contbox img
@@ -57,7 +71,7 @@ function setTyped(sName, sCont, spVal) {
     backSpeed: 0,
     // 반복여부(true/false)
     loop: false,
-    startDelay: 600,
+    startDelay: 400,
 
     onComplete: (self) => {
       // console.log(7777, self.el.id);
@@ -68,139 +82,19 @@ function setTyped(sName, sCont, spVal) {
   }); ////// typed /////////////
 }
 
-const startLetterFn = {
-  "first-area": () => {
-    // 1. first area 파트
-    setTyped(
-      "#typed1",
-      [
-        selData.first
-        // `^1000The smallest planet in our solar system and^100\nnearest to the Sun, Mercury is only slightly ^100\nlarger than Earth's Moon.`,
-      ],
-      4
-    );
-    setTyped("#typed2", [
-      // planetKey
-      `Mercury`
-    ], 30);
-  },
-  "introduction-area": () => {
-    // 2. Introduction 파트
-    setTyped(
-      "#typed3",[`introduction`], 1);
-    setTyped(
-      "#typed4",
-      [
-        selData.introduction
-        // `^1000surface temperatures : 800°F(430°C) ~ -290°F(-180°C)^100\nthe fastest planet, zipping around the Sun every^100\n88 Earth days.`,
-      ],
-      1
-    );
-  },
-  "namesake-area": () => {
-    // 3. namesake 파트
-    setTyped("#typed5", [`namesake`], 1);
-    setTyped(
-      "#typed6",
-      [
-        selData.namesake
-        // `^1000named for the swiftest of the ancient Roman gods.`
-      ],
-      1
-    );
-  },
-  "potential-area": () => {
-    // 4. Potential for Life 파트
-    setTyped("#typed7", [`Potential for Life`], 1);
-    setTyped(
-      "#typed8",
-      [
-        selData.potential
-        // `^1000Mercury's environment is not conducive to life as we know it. \nThe temperatures and solar radiation that characterize this planet\nare most likely too extreme for organisms to adapt to.`,
-      ],
-      1
-    );
-  },
-  "structure-area": () => {
-    // 5. Structure 파트
-setTyped("#typed9", [`Structure`], 1);
+
 setTyped(
-  "#typed10",
+  "#typed1",
   [
-    selData.structure
-    // `^1000Mercury is the second densest planet, after Earth.\nIt has a large metallic core with a radius of about 1,289 miles (2,074 kilometers),\nabout 85% of the planet's radius. There is\nevidence that it is partly molten or liquid. \nMercury's outer shell, comparable to Earth's outer\nshell (called the mantle and crust), is only\nabout 400 kilometers (250 miles) thick.`,
+    selData.first
+    // `^1000The smallest planet in our solar system and^100\nnearest to the Sun, Mercury is only slightly ^100\nlarger than Earth's Moon.`,
   ],
-  1
+  3
 );
-  }
-};
-
-
-
-// 위치에 따른 타이핑 스크롤 액션 //////////////////////////////////////////
-
-// 1. 대상선정:
-// (1) 이벤트 대상 : window
-// (2) 변경 대상 : .scroll-act
-const scrollAct = myFn.qsa(".scroll-act");
-
-// console.log("대상:", scrollAct)
-
-// 스크롤 등장요소의 위치값 담기
-// ooffsetTop은 맨 위에서부터 요소의 위치값
-// 배열변수에 순서대로 담는다.
-const posEl = [];
-scrollAct.forEach((el, idx) => (posEl[idx] = el.offsetTop));
-
-// console.log("위치값:", posEl);
-
-// 2. 이벤트 설정하기 //
-// (1) 스크롤시 요소등장 함수호출
-myFn.addEvt(window, "scroll", showEl);
-
-// (2) 섹션에 따라 행성 이동 함수 호출 --> 추후 추가
-myFn.addEvt(window, "scroll", moving);
-
-// 기준값 만들기 : 화면 높이값을 사용(화면의 2/3)
-// -> 함수를 바로 호출할 수 있도록 수정
-const CRITERIA = 0;
-// console.log("기준값:", CRITERIA);
-
-// 타이핑 애니메이션 한번만 적용하기 //
-let typing = false;
-// 타이핑 효과 시간상수
-const TIME_TYPING = 3000;
-
-let beforeId = [];
-
-// 3. 함수만들기 //
-// (1) 요소 등장 함수
-function showEl() {
-  // (1-1) 함수호출확인
-  // console.log("나야나!!",window.scrollY);
-
-  // (1-2) 타이핑 애니 한번만 적용하기
-  // if (typing) return;// 함수 나가기
-  // typing = true;
-  scrollAct.forEach((el) => {
-    // 각 등장요소의 바운딩 top값
-    /* let bcrVal = myFn.getBCR(el); */
-    // console.log("등장요소 바운딩 top값은?:",bcrVal,el.getBoundingClientRect());
-
-    // 화면의 2/3위치에서 클래스 넣기(등장)
-    /* if (bcrVal < CRITERIA)  */{
-      if (beforeId.includes(el.id)) return;
-      beforeId.push(el.id);
-      // console.log(el.id);
-
-      startLetterFn[el.id]();
-
-      // typed 플러그인 적용하기!
-      // 1. 첫 등장 파트
-    } // if //
-  }); ////// forEach /////
-} ////// showEl함수 //////
-
+setTyped("#typed2", [
+  // planetKey
+  `Mercury`
+], 15);
 
 // 행성 이미지 이동 함수 /////////////////////////////////////////////////////
 
@@ -209,7 +103,7 @@ function showEl() {
 const move = myFn.qsa(".move");
 // (2) 변경대상 : .spin-box
 const movePlanet = myFn.qs(".planetImg");
-console.log("변경대상",movePlanet);
+// console.log("변경대상",movePlanet);
 
 // 이벤트 대상 위치값 담기
 const moveEl = [];
