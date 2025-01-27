e0fb95c7db7d30cb990d2fa4923a86375f821b87
Hyeonseok Yang <gustjr8293@naver.com>
2025-01-27 11:40:28 +0900

    퀵 메뉴바 만드는 중

--- 02.구현소스/js/sub-venus.js
+++ 02.구현소스/js/sub-venus.js
@@ -4,54 +4,16 @@
 import myFn from "./my_function.js";
 
 
-// import planetData from "./data_sub.json" with{type:'json'};
-// console.log(planetData);
+import planetData from "./data_sub.json" with{type:'json'};
+console.log(planetData);
 
 // 행성키 변수 : url?planet=venus
 // let planetKey = location.search.split('=')[1];
-// let planetKey = 'VENUS';
-// const selData = planetData [planetKey];
-// console.log(selData);
+let planetKey = 'VENUS';
+const selData = planetData [planetKey];
+console.log(selData);
 
-let planetData = null
-
-// json 파일 경로
-const jsonFilePath = "./js/data_sub.json";
-
-// json 파일로드
-fetch(jsonFilePath)
-.then((response) => {
-  if (!response.ok){
-    throw new Error ("JSON 파일을 불러오는데 실패했습니다.")
-  }
-  return response.json(); // JSON 데이터를 파싱
-})
-.then((data) => {
-  planetData = data;
-  console.log("json 데이터 로드 완료:",planetData);
-})
-.catch((error) => {
-  console.error("에러 발생:", error);
-})
-
-// 클릭 이벤트 설정
-$(document).ready(function (){
-  // 클릭 이벤트 설정
-  $(".header-nav li a").click(function(e){
-    e.preventDefault(); // 기본 동작 방지
-
-    // 클릭한 행성의 키 가져오기
-    const planetKey = $(this).data("name").toUpperCase();
-    // 데이터 확인 후 업데이트
-    if (planetData && planetData[planetKey]){
-      const selectedPlanet = planetData[planetKey];
-
-      // HTML 업데이트
-      $(".desc-desc p").text(selectedPlanet.introduction) // 내용
-    }
-    else {
-      console.error("해당 키에 대한 데이터를 찾을 수 없습니다.")
-    }
-  })
-})
-
+if (selData) {
+  document.querySelector(".desc-tit h1").textContent = selData.name.toUpperCase();;
+  document.querySelector(".desc-desc p").textContent = selData.first;
+}
