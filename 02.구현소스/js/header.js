0f455834f6588f7ff686eaf0bc65edaaaf463af2
ToffeeSta <Wjdgnsrh@gmail.com>
2025-02-01 22:14:03 +0900

    0201 update

--- 02.구현소스/js/header.js
+++ 02.구현소스/js/header.js
@@ -1,4 +1,8 @@
 const head = document.getElementById("header");
+const ham = document.querySelector('.hammenu'),
+fi = document.querySelector('.ham > span:nth-of-type(1)'),
+se = document.querySelector('.ham > span:nth-of-type(2)'),
+th = document.querySelector('.ham > span:nth-of-type(3)');
 
 let pos = { y: 0, y2: 0, status: true };
 
@@ -16,3 +20,21 @@ window.addEventListener("scroll", function () {
     head.classList.remove("active");
   }
 });
+
+function on(){
+  ham.classList.toggle('on');
+  if(ham.className.includes('on')){
+      fi.style.cssText = 'top: 50%; transform: rotate(45deg)';
+      se.style = 'opacity: 0;';
+      th.style.cssText = 'top: 50%; transform: rotate(-45deg)';
+      $('.ham-drop-box').animate({ left: '0' }, 300);
+  } else{
+      fi.style = 'top: 0%';
+      se.style = 'top: 50%';
+      th.style = 'top: 100%';
+      $('.ham-drop-box').animate({ left: '-100%' }, 300);
+  }  
+}
+
+// 햄버거 버튼 클릭시 on함수 실행
+ham.addEventListener('click',on);
