1529af669b1b5df2d96e566bc045dfe7d66b5b39
ToffeeSta <Wjdgnsrh@gmail.com>
2025-02-05 15:05:02 +0900

    ㅇㅇ

--- 02.구현소스/js/main.js
+++ 02.구현소스/js/main.js
@@ -40,12 +40,6 @@ fetch("./js/data_main.json")
     new Swiper(".mySwiper", {
       loop: true,
       speed: 1000,
-      allowTouchMove: false,
-      // mousewheel : true,
-      // autoplay : {  // 자동 슬라이드 설정
-      //   delay : 3000,
-      //   disableOnInteraction : false,
-      // },
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -62,6 +56,7 @@ fetch("./js/data_main.json")
         const modelingValue = item.getAttribute("data-modeling");
 
         init3D(modelingValue); // 클릭 시 3D 초기화 함수 호출
+
       };
     });
 
